837b13484fbe0b8796b8b5e5c1c692d1b707e28b
Seidi A Mohammad <seidiali21@yahoo.com>
2020-10-05 23:25:50 -0400

    fivth commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,11 +12,14 @@ const margin = {
 const width = svgWidth - margin.left - margin.right;
 const height = svgHeight - margin.top - margin.bottom;
 
+runApp();
+
 // Create an SVG wrapper, append an SVG group that will hold our chart,
 // and shift the latter by left and top margins.
 const svg = d3
   .select("#scatter")
   .append("svg")
+  // .style('background','lightgray')
   .attr("width", svgWidth)
   .attr("height", svgHeight + 40); // extra padding for third label
 
@@ -28,12 +31,14 @@ const chartGroup = svg.append("g")
 let chosenXAxis = "poverty";
 let chosenYAxis = "healthcare";
 
-(async function(){
+var data;
 
+async function runApp(){
   // Import Data
   const stateData = await d3.csv("assets/data/data.csv");
-
+  data = stateData;
   // Parse Data/Cast as numbers
+
   stateData.forEach(function(data) {
     data.poverty    = +data.poverty;
     data.healthcare = +data.healthcare;
@@ -42,6 +47,179 @@ let chosenYAxis = "healthcare";
     data.obesity    = +data.obesity;
     data.income     = +data.income;
   });
+
+ // function xScale(data, chosenXAxis) {
+ //     let chosenValue = data.map(obj => obj[chosenXAxis]);
+ //     let xMin = d3.min(chosenValue)*0.90;
+ //     let xMax = d3.max(chosenValue)*1.10;
+ //     let xLine = d3.scaleLinear().domain([xMin, xMax]).range([0,960]);
+ //     let xAxis = d3.axisBottom(xLine);
+ //     svg.append('g').call(xAxis);
+ //     console.log(xMin,xMax);
+ // }
+
+ // function yScale(data, chosenYAxis) {
+ //     let chosenValue = data.map(obj => obj[chosenYAxis]);
+ //    let yMin = d3.min(chosenValue)*0.90;
+ //     let yMax = d3.max(chosenValue)*1.10;
+ //     let yLine = d3.scaleLinear().domain([yMin, yMax]).range([0,500]);
+ //     let yAxis = d3.axisBottom(yLine);
+  //    svg.append('g').call(yAxis);
+  //    console.log(yMin,yMax);
+ // } 
+
+function xScale(csvData, chosenXAxis) {
+  // create scales
+  let xLinearScale = d3.scaleLinear()
+    .domain([d3.min(csvData, d => d[chosenXAxis]) * 0.9,
+      d3.max(csvData, d => d[chosenXAxis]) * 1.1
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+
+}
+
+
+function yScale(csvData, chosenYAxis) {
+  // create scales
+  let yLinearScale = d3.scaleLinear()
+    .domain([d3.min(csvData, d => d[chosenYAxis]) - 1,
+      d3.max(csvData, d => d[chosenYAxis]) + 1
+    ])
+    .range([height, 0]);
+
+  return yLinearScale;
+}
+
+
+function renderXAxes(newXScale, xAxis) {
+  let bottomAxis = d3.axisBottom(newXScale);
+
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+
+  return xAxis;
+}
+
+
+function renderYAxes(newYScale, yAxis) {
+  let leftAxis = d3.axisLeft(newYScale);
+
+  yAxis.transition()
+    .duration(1000)
+    .call(leftAxis);
+
+  return yAxis;
+}
+ 
+
+function renderXCircles(circlesGroup, newXScale, chosenXaxis) {
+
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]));
+
+  return circlesGroup;
+}
+
+function renderYCircles(circlesGroup, newYScale, chosenYaxis) {
+
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cy", d => newYScale(d[chosenYAxis]));
+
+  return circlesGroup;
+}
+
+
+function renderXText(circlesGroup, newXScale, chosenXaxis) {
+
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("dx", d => newXScale(d[chosenXAxis]));
+
+  return circlesGroup;
+}
+
+function renderYText(circlesGroup, newYScale, chosenYaxis) {
+
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("dy", d => newYScale(d[chosenYAxis])+5);
+
+  return circlesGroup;
+}
+
+// format number to USD currency
+let formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
+
+
+
+function updateToolTip(circlesGroup, chosenXAxis, chosenYAxis) {
+
+  let xpercentsign = "";
+  let xlabel = "";
+  if (chosenXAxis === "poverty") {
+    xlabel = "Poverty";
+    xpercentsign = "%";
+  } else if (chosenXAxis === "age"){
+    xlabel = "Age";
+  } else {
+    xlabel = "Income";
+  }
+
+  let ypercentsign = "";
+  let ylabel = "";
+  if (chosenYAxis === "healthcare") {
+    ylabel = "Healthcare";
+    ypercentsign = "%";
+  } else if (chosenYAxis === "smokes"){
+    ylabel = "Smokes";
+    ypercentsign = "%";
+  } else {
+    ylabel = "Obesity";
+    ypercentsign = "%";
+  }
+
+  const toolTip = d3.tip()
+    .attr("class", "d3-tip")
+    .offset([50, -75])
+    .html(function(d) {
+      if (chosenXAxis === "income"){
+        let incomelevel = formatter.format(d[chosenXAxis]);
+
+        return (`${d.state}<br>${xlabel}: ${incomelevel.substring(0, incomelevel.length-3)}${xpercentsign}<br>${ylabel}: ${d[chosenYAxis]}${ypercentsign}`)
+      } else {
+        return (`${d.state}<br>${xlabel}: ${d[chosenXAxis]}${xpercentsign}<br>${ylabel}: ${d[chosenYAxis]}${ypercentsign}`)
+      };
+    });
+
+  circlesGroup.call(toolTip);
+
+  // mouseover event
+  circlesGroup.on("mouseover", function(data) {
+      toolTip.show(data, this);
+      // trying to highlight chosen circle
+      // circlesGroup.append("circle")
+      //   .attr("cx", d3.event.pageX)
+      //   .attr("cy", d3.event.pageY)
+      //   .attr("r", 15)
+      //   .attr("stroke", "black")
+      //   .attr("fill", "none");
+  })
+    // onmouseout event
+    .on("mouseout", function(data) {
+        toolTip.hide(data, this);
+    });
+
+return circlesGroup;
+}
+
 
   // Initialize scale functions
   let xLinearScale = xScale(stateData, chosenXAxis);
@@ -265,4 +443,4 @@ let chosenYAxis = "healthcare";
     }
   });
 
-})
+}
